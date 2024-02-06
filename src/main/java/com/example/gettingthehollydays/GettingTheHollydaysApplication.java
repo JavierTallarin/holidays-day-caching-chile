7a237a4764708acc95fc6a172a395788217d1df4
javierTallarin <javier1bravo@gmail.com>
2024-02-05 23:58:20 -0300

    empty spring boot 3 project

---
+++ src/main/java/com/example/gettingthehollydays/GettingTheHollydaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.gettingthehollydays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingTheHollydaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingTheHollydaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gettingthehollydays/GettingTheHollydaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gettingthehollydays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingTheHollydaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
